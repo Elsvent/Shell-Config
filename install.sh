28c4fae51adb988a57bb0fd5253c7656290d2885
Elsvent Hong <elsvent.hong@prophetstor.com>
2018-04-27 09:57:10 +0900

    add zsh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 sudo apt update && sudo apt install zsh -y && chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sed 's/robbyrussell/af-magic/g' ~/.zshrc
+zsh
 #Set git environment
 #git config --global user.email "elsvent@gmail.com"
 #git config --global user.name "Elsvent Hong"
