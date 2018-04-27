fffc0fced02425817f379f5726556370410ce6d1
Elsvent Hong <elsvent.hong@prophetstor.com>
2018-04-27 09:55:37 +0900

    for server zsh deploy

--- install.sh
+++ install.sh
@@ -1,35 +1,10 @@
-CONFIG_HOME=.myconfig
+#!/bin/bash
 
-warn(){
-	echo "$1" >&2
-}
-
-die(){
-	warn "$1"
-	exit 1
-}
-
-[ -e "~/.myconfig" ] && die "~/myconfig already exists."
-
-cd ~
-
-#Checkout my config
-git clone git://github.com/Elsvent/Shell-Config.git "$CONFIG_HOME"
-
-#install vundle for vim
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-
-#install oh-my-zsh
-curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-
-ln -s $CONFIG_HOME/.vim/.vimrc .vimrc
-ln -s $CONFIG_HOME/.zshrc .zshrc
-ln -s $CONFIG_HOME/.tmux.conf .tmux.conf
-ln -s $CONFIG_HOME/hg/.hgrc .hgrc
-
+sudo apt update && sudo apt install zsh -y && chsh -s /bin/zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sed 's/robbyrussell/af-magic/g' ~/.zshrc
 #Set git environment
-git config --global user.email "elsvent@gmail.com"
-git config --global user.name "Elsvent Hong"
+#git config --global user.email "elsvent@gmail.com"
+#git config --global user.name "Elsvent Hong"
 
 echo "Configuration files has been installed."
-cd "$CONFIG_HOME"
